9e36b881b857f1f05e0a0c16da3d6fbd228b92df
rohit87099 <155307408+rohit87099@users.noreply.github.com>
2024-09-18 09:33:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,6 +26,12 @@ TUT_VID = os.environ.get("TUT_VID", "https://t.me/Tutorial_modijiurl/3")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002486154523"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002001392051"))
+
+if FORCE_SUB_CHANNEL > FORCE_SUB_CHANNEL2:
+    temp = FORCE_SUB_CHANNEL2 
+    FORCE_SUB_CHANNEL2 = FORCE_SUB_CHANNEL
+    FORCE_SUB_CHANNEL = temp
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
