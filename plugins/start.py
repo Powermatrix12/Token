47a3d45ed945e02b42c5eed5199c22b4a28e7de8
rohit87099 <155307408+rohit87099@users.noreply.github.com>
2024-09-18 10:23:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -126,8 +126,8 @@ async def start_command(client: Client, message: Message):
                 except:
                     pass
 
-            SD = await message.reply_text("Baka! Files will be deleted After 300 seconds. Save them to the Saved Message now!")
-            await asyncio.sleep(300)
+            SD = await message.reply_text("Baka! Files will be deleted After 24 hrs. Save them to the Saved Message now!")
+            await asyncio.sleep(86400)
 
             for snt_msg in snt_msgs:
                 try:
