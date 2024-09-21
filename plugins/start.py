9370ab8aa0cc4066d4dc3b2f8bf7a4addb574772
rohit87099 <155307408+rohit87099@users.noreply.github.com>
2024-09-21 18:50:16 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -202,9 +202,8 @@ async def not_joined(client: Client, message: Message):
     except IndexError:
         pass
 
-    await message.reply_photo(
-    photo=FORCE_PIC,
-        caption = FORCE_MSG.format(
+    await message.reply_text(
+        text = FORCE_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
