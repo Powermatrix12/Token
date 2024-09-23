dfc2f53b5a5d91fffe2b6d826f8506dc9c8eeecc
Powermatrix12 <167418825+Powermatrix12@users.noreply.github.com>
2024-09-23 15:14:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7201537576:AAHSiwZhJPLmP6iPfRiTk3azgB1EVBR7n2o")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7201537576:AAGFO9TDlPGkR7ECX1ISX8yU_VSCW-6KnGw")
 APP_ID = int(os.environ.get("APP_ID", "26590590"))
 API_HASH = os.environ.get("API_HASH", "4805ee1d57b1be7f5135e736c816a2d1")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001992443365"))
